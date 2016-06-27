056fa1f831f15223473c5dac19d06872069dd993
Huw Jones <huwcbjones@gmail.com>
2016-06-27 12:46:04 +0100

    Added users table

---
+++ beta/config/schema/users.sql
@@ -0,0 +1,14 @@
+#
+# Users Table
+#
+CREATE TABLE users (
+  id int(11) NOT NULL auto_increment,
+  username varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  firstName varchar(255) NOT NULL,
+  lastName varchar(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created DATETIME,
+  modified DATETIME,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
