a984604e598e6dac5125bc4966ae14d735c2e644
Huw Jones <huwcbjones@gmail.com>
2016-07-04 16:36:52 +0100

    Loaded news

--- beta/src/Controller/NewsController.php
+++ beta/src/Controller/NewsController.php
@@ -5,13 +5,31 @@
  */
 
 namespace SUSC\Controller;
+use Cake\ORM\TableRegistry;
 
-
+/**
+ * News Controller
+ *
+ * @property \SUSC\Model\Table\NewsTable $News
+ */
 class NewsController extends AppController
 {
+    public function initialize()
+    {
+        parent::initialize();
+        $this->Auth->allow();
+        $this->News = TableRegistry::get('News');
+    }
 
     public function index()
     {
+        $this->paginate = [
+            'contain' => ['news']
+        ];
+        $news = $this->paginate($this->News);
+
+        $this->set(compact('news'));
+        $this->set('_serialize', ['news']);
     }
 
     public function view($fixtureID)
