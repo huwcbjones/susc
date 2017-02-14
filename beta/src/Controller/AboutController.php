b0e463c1edcd1ae48e48bef24ac7e26e9d9b8bdb
Huw Jones <huwcbjones@gmail.com>
2017-02-14 23:20:34 +0000

    Moved to custom markdown extension class

--- beta/src/Controller/AboutController.php
+++ beta/src/Controller/AboutController.php
@@ -8,7 +8,7 @@ namespace SUSC\Controller;
 
 
 use Cake\ORM\TableRegistry;
-use cebe\markdown\GithubMarkdown;
+use huwcbjones\markdown\GithubMarkdownExtended;
 
 class AboutController extends AppController
 {
@@ -23,15 +23,15 @@ class AboutController extends AppController
 
     public function club()
     {
-        $parser = new GithubMarkdown();
+        $parser = new GithubMarkdownExtended();
         $this->set('content', $parser->parse($this->Static->find('club')->first()->value));
     }
 
     public function committee(){
-        $this->set('committee', $this->Committee->find('published'));
+        $this->set('committee', $this->Committee->find('published')->order(['display_order' => 'ASC']));
     }
 
     public function coaches(){
-        $this->set('coaches', $this->Coaches->find('published'));
+        $this->set('coaches', $this->Coaches->find('published')->order(['display_order' => 'ASC']));
     }
 }
