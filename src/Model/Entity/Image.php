a45de9db5cf022ce5e507ba198dd46d742abd2ad
Huw Jones <huwcbjones@gmail.com>
2017-02-16 21:52:41 +0000

    Created gallery/images tables. Moved to image table in home.ctp. Removed gallery/* as on new table using store/*

---
+++ src/Model/Entity/Image.php
@@ -0,0 +1,39 @@
+<?php
+namespace SUSC\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Image Entity
+ *
+ * @property string $id
+ * @property string $title
+ * @property string $extension
+ * @property string $copyright
+ * @property \Cake\I18n\Time $created
+ * @property \Cake\I18n\Time $modified
+ * @property bool $status
+ *
+ * @property \SUSC\Model\Entity\Gallery[] $galleries
+ */
+class Image extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        '*' => true,
+        'id' => false
+    ];
+
+    protected function _getPath(){
+        return "/images/store/" . $this->id . '.' . $this->extension;
+    }
+}
