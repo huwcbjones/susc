55f291889af6ec209ac0cf867c44a5d56ac81f1f
Huw Jones <huwcbjones@gmail.com>
2017-02-23 13:03:45 +0000

    Added user Table/Entityˆ

---
+++ src/Model/Entity/User.php
@@ -0,0 +1,53 @@
+<?php
+namespace SUSC\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * User Entity
+ *
+ * @property string $id
+ * @property string $username
+ * @property string $email_address
+ * @property string $first_name
+ * @property string $last_name
+ * @property \Cake\I18n\Time $activation_date
+ * @property string|resource $password
+ * @property bool $is_active
+ * @property bool $is_enable
+ * @property bool $change_password
+ * @property \Cake\I18n\Time $created
+ * @property \Cake\I18n\Time $modified
+ *
+ * @property \App\Model\Entity\Article[] $articles
+ */
+class User extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        '*' => true,
+        'id' => false
+    ];
+
+    /**
+     * Fields that are excluded from JSON versions of the entity.
+     *
+     * @var array
+     */
+    protected $_hidden = [
+        'password'
+    ];
+
+    protected function _getFullname(){
+        return $this->first_name . ' ' . $this->last_name;
+    }
+}
